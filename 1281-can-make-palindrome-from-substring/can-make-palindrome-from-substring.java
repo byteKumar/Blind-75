b32c817faf39fa78809b7f72c322a12c4063b86b
Chaman Kumar <54892093+byteKumar@users.noreply.github.com>
2025-08-02 15:48:50 -0400

    Time: 7 ms (98.48%) | Memory: 91.7 MB (97.73%) - LeetSync

--- 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.java
+++ 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
         int n = s.length();
-        int[] prefix = new int[n + 1];
-        for (int i = 0; i < n; i++) {
+        int [] prefix = new int[n+1];
+        for(int i=0;i<n;i++){
             int bit = 1 << (s.charAt(i) - 'a');
-            prefix[i + 1] = prefix[i] ^ bit;
+            prefix[i+1] = prefix[i]^bit;
+        }
+        
+        List<Boolean> ans = new ArrayList<>(queries.length);
+        for(int[] q:queries){
+            int l=q[0], r=q[1], k=q[2];
+            int mask = prefix[r+1]^prefix[l];
+            int count = Integer.bitCount(mask);
+            ans.add((count/2)<=k);
         }
 
-        List<Boolean> ans = new ArrayList<>(queries.length);
-        for (int[] q : queries) {
-            int l = q[0], r = q[1], k = q[2];
-            int mask = prefix[r + 1] ^ prefix[l];
-            int oddCount = Integer.bitCount(mask);
-            ans.add((oddCount / 2) <= k);
-        }
-        return ans;   
+        return ans;
     }
 }
