3691ec671b035edfbb5a57d95696f87159f6c999
Chaman Kumar <54892093+byteKumar@users.noreply.github.com>
2025-10-13 18:32:18 -0400

    Time: 3 ms (39.08%) | Memory: 45.5 MB (93.37%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,33 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-
-        int n = nums.length;
-        int max = 0;
-
-        if (Arrays.equals(nums, new int[]{4, 2, 0, 0, 1, 1, 4, 4, 4, 0, 4, 0})) {
+        int maxIndex = 0;
+        if(nums.length == 1){
             return true;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (i != n-1 && nums[i] == 0) {
+        for(int i=0;i<nums.length;i++){
+            if(maxIndex < i) {
                 return false;
             }
-            if (nums[i] >= (nums.length - 1) - i) {
-                return true;
-            }
-            int start = i+1;
-            int j = start + nums[i] -1;
-            max = 0;
-            while (start <= j) {
-                if (nums[start] >= max) {
-                    max = nums[start];
-                    i = start;
-                    i--;
-                }
-                start++;
-            }
+            maxIndex = Math.max(maxIndex, nums[i] + i);
         }
+
         return true;
-        
     }
 }
